a103ef0682660b492bc512c57d419558e5bd5dbe
ABRUIZ25 <89878760+ABRUIZ25@users.noreply.github.com>
2022-05-18 10:44:15 -0400

    made the code better

--- routes/blogs.js
+++ routes/blogs.js
@@ -6,8 +6,8 @@ let blogs = require('../public/Blogs')
 router.get('/allblogs', function (req, res, next) {
     let blogPosts = blogs.blogPosts
     const BlogsDate = []
-    const ascBlogDate = []
-    const descBlogDate = []
+
+
     let sort = req.query.sort
 
 
@@ -18,16 +18,19 @@ router.get('/allblogs', function (req, res, next) {
 
 
     if (sort === 'asc') {
-        BlogsDate.sort(function (a, b) { BlogsDate.push(a - b) })
-        ascBlogDate.push(BlogsDate)
+
+
+
+        const ascBlogDate = BlogsDate.sort(function (a, b) { return a - b })
+
 
         res.json(ascBlogDate)
         console.log('if desc statement', 'asc')
         console.log(ascBlogDate)
 
     } else if (sort === 'desc') {
-        BlogsDate.sort(function (a, b) { return b - a })
-        descBlogDate.push(BlogsDate)
+        const descBlogDate = BlogsDate.sort(function (a, b) { return b - a })
+
 
         res.json(descBlogDate)
         console.log('if desc statement', 'desc')
